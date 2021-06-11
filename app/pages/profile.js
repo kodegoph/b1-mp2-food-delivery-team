26da20ffce59cc9fd20ca95998c4e453c87bb3a6
Iamgelo <gelo.soliven01@gmail.com>
2021-06-11 09:36:54 +0800

    first commit

---
+++ app/pages/profile.js
@@ -0,0 +1,18 @@
+// Just another page.
+import Theme from '../components/Theme';
+import indexStyle from '../styles/index.scss';
+
+
+
+const Profile = () => (
+  
+    <>
+    <Theme>
+    <style dangerouslySetInnerHTML={{ __html: indexStyle }} />
+    
+   </Theme>
+    </>
+
+);
+
+export default Profile;
