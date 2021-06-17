9cfa519b905408fd940831b65e701f7b39d50c77
Iamgelo <gelo.soliven01@gmail.com>
2021-06-17 12:52:54 +0800

    "update 6/7/21"

--- components/cards/Modals.js
+++ components/cards/Modals.js
@@ -4,7 +4,7 @@ import { Modal } from 'react-responsive-modal';
 import Button from 'react-bootstrap'
 import Container from 'react-bootstrap'
 import Row from 'react-bootstrap'
-
+import Image from 'next/image'
 
 const App = () => {
   const [open, setOpen] = useState(false);
@@ -13,62 +13,106 @@ const App = () => {
   const onCloseModal = () => setOpen(false);
 
   return (
+        <>
     <div className="h-100">
         <h5><a className="text-decoration-none cursor-pointer" onClick={onOpenModal}>View all</a></h5>
       <Modal open={open} onClose={onCloseModal} center>
         <h2 className="pt-4">All Favourite Korean Foods</h2>
-       
-        {/* <Row>
-    <div className="col">
-      1 of 3
-    </div>
-    <div className="col-6">
-      2 of 3 (wider)
-    </div>
-    <div className="col">
-      3 of 3
-    </div>
-    </Row>
-    <Row>
-    <div className="col">
-      1 of 3
-    </div>
-    <div className="col-5">
-      2 of 3 (wider)
-    </div>
-    <div className="col">
-      3 of 3
-    </div>
-    </Row>
+        <div className='row g-5'>
+          <div className="col-4">
+            <div className="card"  style={{width : '18rem'}}>
+            <img src="http://lorempixel.com/640/480/sports" alt="placeholder image"/>
+              <div className="card-body">
+                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              </div>
+           
+             </div>
+            </div>
+            <div className="col-4">
+            <div className="card"  style={{width : '18rem'}}>
+            <img src="http://lorempixel.com/640/480/food" alt="placeholder image"/>
+              <div className="card-body">
+                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              </div>
+           
+             </div>
+            </div>
+            <div className="col-4">
+            <div className="card"  style={{width : '18rem'}}>
+            <img src="http://lorempixel.com/640/480/food" alt="placeholder image"/>
+              <div className="card-body">
+                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              </div>
+           
+             </div>
+            </div>
+          </div>
+          <div className='row'>
+          <div className="col-4">
+            <div className="card"  style={{width : '18rem'}}>
+            <img src="http://lorempixel.com/640/480/food" alt="placeholder image"/>
+              <div className="card-body">
+                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              </div>
+           
+             </div>
+            </div>
+            <div className="col-4">
+            <div className="card"  style={{width : '18rem'}}>
+            <img src="http://lorempixel.com/640/480/food" alt="placeholder image"/>
+              <div className="card-body">
+                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              </div>
+           
+             </div>
+            </div>
+            <div className="col-4">
+            <div className="card"  style={{width : '18rem'}}>
+            <img src="http://lorempixel.com/640/480/food" alt="placeholder image"/>
+              <div className="card-body">
+                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              </div>
+           
+             </div>
+            </div>
+          </div>
+          <div className='row'>
+          <div className="col-4">
+            <div className="card"  style={{width : '18rem'}}>
+            <img src="http://lorempixel.com/640/480/food" alt="placeholder image"/>
+              <div className="card-body">
+                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              </div>
+           
+             </div>
+            </div>
+            <div className="col-4">
+            <div className="card"  style={{width : '18rem'}}>
+            <img src="http://lorempixel.com/640/480/food" alt="placeholder image"/>
+              <div className="card-body">
+                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              </div>
+           
+             </div>
+            </div>
+            <div className="col-4">
+            <div className="card"  style={{width : '18rem'}}>
+            <img src="http://lorempixel.com/640/480/food" alt="placeholder image"/>
+              <div className="card-body">
+                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              </div>
+           
+             </div>
+            </div>
+          </div>
 
-    <Row>
-    <div className="col">
-      1 of 3
-    </div>
-    <div className="col-6">
-      2 of 3 (wider)
-    </div>
-    <div className="col">
-      3 of 3
-    </div>
-    </Row>
-    <Row>
-    <div className="col">
-      1 of 3
-    </div>
-    <div className="col-5">
-      2 of 3 (wider)
-    </div>
-    <div className="col">
-      3 of 3
-    </div> 
-    </Row>*/}
-{/*         
-             <Button>Add More Food to the List</Button>  */}
-            
-       <button className="btn-primary">Add new food</button>
+
+       <div className='row d-flex justify-content-center m-5'>
+       <button className="btn-primary col-6 radius-30">Add new food</button>
+       </div>
       </Modal>
     </div>
+      </>
   );
 };
 
