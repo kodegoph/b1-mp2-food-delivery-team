73f2962f90c019e4c809e132ef96dcadb9c38ae4
jamesvilleza <84057160+jamesvilleza@users.noreply.github.com>
2021-06-18 13:56:13 +0800

    login page design

--- components/LoginForm.js
+++ components/LoginForm.js
@@ -1,6 +1,5 @@
 import Link from "next/link";
 import App from "next/app";
-
 class Login extends App {
     constructor(props) {
         super(props);
@@ -33,67 +32,50 @@ class Login extends App {
     render() {
         const {username, password, error} = this.state;
         return (
-            <div className="container">
-                <div className="row no-gutter">
-                    <div className="d-none d-md-flex col-md-4 col-lg-6 bg-image"></div>
-                    <div className="col-md-8 col-lg-6">
-                        <div className="login d-flex align-items-center py-5">
-                            <div className="container border-3 rounded">
-                                <div className="row">
-                                    <div className="col-md-9 col-lg-8 mx-auto">
-                                        <h3 className="login-heading mb-4">
-                                            Welcome back!
-                                        </h3>
-                                        <form onSubmit={this.handleSubmit}>
-                                            <div className="form-label-group">
-                                                <input
-                                                    type="Name"
-                                                    name="username"
-                                                    className="form-control"
-                                                    placeholder="Username"
-                                                    required
-                                                    autofocus
-                                                    value={username}
-                                                    onChange={
-                                                        this.handleInputChange
-                                                    }
-                                                />
-                                                <label htmlFor="inputName">
-                                                    Username
-                                                </label>
-                                            </div>
-
-                                            <div className="form-label-group">
-                                                <input
-                                                    type="password"
-                                                    name="password"
-                                                    className="form-control"
-                                                    placeholder="Password"
-                                                    required
-                                                    value={password}
-                                                    onChange={
-                                                        this.handleInputChange
-                                                    }
-                                                />
-                                                <label htmlFor="inputPassword">
-                                                    Password
-                                                </label>
-                                            </div>
-                                            {error && (
-                                                <div className="error-message">
-                                                    Wrong username and password
-                                                </div>
-                                            )}
-                                            <button
-                                                className="btn btn-lg btn-primary btn-block btn-login text-uppercase font-weight-bold mb-2"
-                                                type="submit">
-                                                Sign in
-                                            </button>
-                                        </form>
-                                    </div>
-                                </div>
+            <div className="container-fluid vh-100 vw-100 login">
+                <div className="row">
+                    <div className="col-6 col-lg-4 mx-auto py-auto login-card">
+                        <h4 className="login-heading mb-5">
+                            Sign in to continue
+                        </h4>
+                        <form onSubmit={this.handleSubmit}>
+                            <div className="form-label-group">
+                                <input
+                                    type="Name"
+                                    name="username"
+                                    className="form-control"
+                                    placeholder="Username"
+                                    required
+                                    autoFocus
+                                    value={username}
+                                    onChange={this.handleInputChange}
+                                />
+                                <label htmlFor="inputName">Username</label>
                             </div>
-                        </div>
+
+                            <div className="form-label-group">
+                                <input
+                                    type="password"
+                                    name="password"
+                                    className="form-control"
+                                    placeholder="Password"
+                                    required
+                                    value={password}
+                                    onChange={this.handleInputChange}
+                                />
+                                <label htmlFor="inputPassword">Password</label>
+                            </div>
+                            {error && (
+                                <div className="error-message">
+                                    Wrong username and password
+                                </div>
+                            )}
+                            <button
+                                className="btn btn-lg btn-primary btn-block btn-login text-uppercase font-weight-bold mb-2"
+                                type="submit">
+                                Sign in
+                            </button>
+                        </form>
                     </div>
                 </div>
             </div>
