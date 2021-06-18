440f6f1cd5668351faba24f276dd5162f6b64636
jamesvilleza <84057160+jamesvilleza@users.noreply.github.com>
2021-06-18 21:04:00 +0800

    table format

---
+++ pages/general.js
@@ -0,0 +1,11 @@
+import CustomerTable from "../components/CustomerTable";
+
+const general = () => {
+    return (
+        <div className="me-main">
+            <CustomerTable />
+        </div>
+    );
+};
+
+export default general;
