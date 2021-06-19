7e00c0f9601feaba2dfea019de51b8462a3cc597
Iamgelo <gelo.soliven01@gmail.com>
2021-06-19 18:00:43 +0800

    push error fix

---
+++ _static/_next/static/chunks/pages/auth/login-b42a922789ae9b1a990a.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[344],{3916:function(n,u,t){"use strict";t.r(u);var _=t(5893),r=t(4228);u.default=function(){return(0,_.jsx)(r.Z,{})}},28:function(n,u,t){(window.__NEXT_P=window.__NEXT_P||[]).push(["/auth/login",function(){return t(3916)}])}},function(n){n.O(0,[774,888,179],(function(){return u=28,n(n.s=u);var u}));var u=n.O();_N_E=u}]);
---
+++ _static/_next/static/chunks/pages/login-5a710df7c75080cf8a3e.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[459],{1162:function(n,u,t){"use strict";t.r(u);var _=t(5893);u.default=function(){return(0,_.jsx)("div",{})}},7156:function(n,u,t){(window.__NEXT_P=window.__NEXT_P||[]).push(["/login",function(){return t(1162)}])}},function(n){n.O(0,[774,888,179],(function(){return u=7156,n(n.s=u);var u}));var u=n.O();_N_E=u}]);
